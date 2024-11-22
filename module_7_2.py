07540a96401d46101b1ccd2d6bada7d9597f7481
EvgeniyEvgenievich <Dedjoze@gmail.com>
2024-11-22 15:48:15 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,14 +1,17 @@
 def custom_write(file_name, strings):
     strings_positions = {}
+    
+    f = open(file_name, 'w', encoding='utf-8')  # Открываем файл
 
-    with open(file_name, 'w', encoding='utf-8') as f:
-        for index, string in enumerate(strings, start=1):
-            start_byte = f.tell()
-            f.write(string + '\n')
-            strings_positions[(index, start_byte)] = string
+    # Цикл для записи строк в файл
+    for index, string in enumerate(strings, start=1):
+        start_byte = f.tell()  # Получаем текущую позицию в байтах
+        f.write(string + '\n')  # Записываем строку в файл с переходом на новую строку
+        strings_positions[(index, start_byte)] = string  # Сохраняем в словарь
+
+    f.close()  # Закрываем файл
 
     return strings_positions
-
 
 # Пример использования
 info = [
@@ -22,4 +25,3 @@ result = custom_write('test.txt', info)
 
 for elem in result.items():
     print(elem)
-
